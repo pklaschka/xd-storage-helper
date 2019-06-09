57a9da89dc6f386200c81b3837cceb7e51878b09
Pablo Klaschka <contact@pabloklaschka.de>
2019-06-09 10:13:43 +0200

    Fixed typescript definitions

--- storage-helper.d.ts
+++ storage-helper.d.ts
@@ -3,45 +3,46 @@
  */
 
 /**
- * The xd-storage-helper module
  * @module xd-storage-helper
+ * @desc The storage helper module
  */
-declare module 'xd-storage-helper' {
+
+/**
+ * @class StorageHelper
+ * The main LocalizationHelper class
+ * @alias module:xd-storage-helper
+ * @static
+ * @hideconstructor
+ */
+declare class StorageHelper {
     /**
-     * @class StorageHelper
-     * The main LocalizationHelper class
-     * @alias module:xd-storage-helper
-     * @static
-     * @hideconstructor
+     * Retrieves a value from storage. Saves default value if none is set.
+     * @param {string} key The identifier
+     * @param {*} defaultValue The default value. Gets saved and returned if no value was previously set for the speciefied key.
+     * @return {Promise<*>} The value retrieved from storage. If none is saved, the `defaultValue` is returned.
      */
-    export default class StorageHelper {
-        /**
-         * Retrieves a value from storage. Saves default value if none is set.
-         * @param {string} key The identifier
-         * @param {*} defaultValue The default value. Gets saved and returned if no value was previously set for the speciefied key.
-         * @return {Promise<*>} The value retrieved from storage. If none is saved, the `defaultValue` is returned.
-         */
-        public static get(key:string, defaultValue:any): Promise<any>;
+    public static get(key: string, defaultValue: any): Promise<any>;
 
-        /**
-         * Saves a certain key-value-pair to the storage.
-         * @param {string} key The identifier
-         * @param {*} value The value that get's saved
-         * @return {Promise<void>}
-         */
-        public static set(key: string, value:any): Promise<void>;
+    /**
+     * Saves a certain key-value-pair to the storage.
+     * @param {string} key The identifier
+     * @param {*} value The value that get's saved
+     * @return {Promise<void>}
+     */
+    public static set(key: string, value: any): Promise<void>;
 
-        /**
-         * Deletes a certain key-value-pair from the storage
-         * @param {string} key The key of the deleted pair
-         * @return {Promise<void>}
-         */
-        public static delete(key: string): Promise<void>;
+    /**
+     * Deletes a certain key-value-pair from the storage
+     * @param {string} key The key of the deleted pair
+     * @return {Promise<void>}
+     */
+    public static delete(key: string): Promise<void>;
 
-        /**
-         * Resets (i.e. purges) all stored settings.
-         * @returns {Promise<void>}
-         */
-        public static reset(): Promise<void>;
-    }
+    /**
+     * Resets (i.e. purges) all stored settings.
+     * @returns {Promise<void>}
+     */
+    public static reset(): Promise<void>;
 }
+
+export = StorageHelper;
